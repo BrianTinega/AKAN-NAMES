9d8037073c7eca573e87bc76aee5a60beb71f188
Brian Tinega Nyang'wara <briantinega@gmail.com>
2020-07-19 22:11:02 +0300

    Add javascript code

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,89 @@
 
 function calculateDay() {
-  var year=document.getElementById("yob")
-var month=parseInt(document.getElementById("mob"))
-var day=parseInt(doument.getElementById("dob"))
-var daysofTheWeek=["Sunday","Monday","Tuesday","Wedneday","Thursday","Friday","Saturday"]
-  var CC=parseInt((year.subString(0,2))+1)
-  var YY=parseInt(year.subString(2,4))
-  var MM=month
-  var DD=day
-  d = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7
-  console.log(d)
-  Math.floor(d);
+var maleNames=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","kwame"];
+var femaleNames=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+var gender=document.getElementById("gender").value;
+var year=document.getElementById("yob").value;
+var month=parseInt(document.getElementById("mob").value);
+var day=parseInt(document.getElementById("dob").value);
+  var CC=parseInt(year.slice(0,2));
+  var YY=parseInt(year.slice(2,4));
+  d =Math.floor(( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(month+1)/10)) + day ) % 7);
+
+if (day<1 || day>31) {
+  document.getElementById("output").innerHTML=("Enter valid date");
+  return;
 }
-if (d===0) {
-  alert("You were born on"+ daysofTheWeek[0] )
+if (month<1 || month>12) {
+  document.getElementById("output").innerHTML=("Enter valid month");
+  return;
 }
-else if (d===1) {
-  alert("You were born on"+ daysofTheWeek[1])
+
+if (year<1 || year>2020) {
+  document.getElementById("output").innerHTML=("Enter valid year");
+  return;
 }
-else if (d===2) {
-  alert("You were born on"+ daysofTheWeek[2])
+if (d==0 && gender=="male") {
+
+  document.getElementById("output").innerHTML=("You were born on Sunday and your Akan name is "+maleNames[0]);
 }
-else if (d===3) {
-  alert("You were born on"+ daysofTheWeek[3])
+if (d==1 && gender=="male") {
+
+  document.getElementById("output").innerHTML=("You were born on Monday and your Akan name is "+maleNames[1]);
 }
-else if (d===4) {
-  alert("You were born on"+ daysofTheWeek[4])
+if (d==2 && gender=="male") {
+
+  document.getElementById("output").innerHTML=("You were born on Tuesday and your Akan name is "+maleNames[2]);
 }
-else if (d===5) {
-  alert("You were born on"+ daysofTheWeek[5])
+if (d==3 && gender=="male") {
+
+  document.getElementById("output").innerHTML=("You were born on Wednesday and your Akan name is "+maleNames[3]);
 }
-else if (d===6) {
-  alert("You were born on"+ daysofTheWeek[6])
+if (d==4 && gender=="male") {
+
+  document.getElementById("output").innerHTML=("You were born on Thursday and your Akan name is "+maleNames[4]);
 }
-calculateDay()
+if (d==5 && gender=="male") {
+
+  document.getElementById("output").innerHTML=("You were born on Friday and your Akan name is "+maleNames[5]);
+}
+if (d==6 && gender=="male") {
+
+  document.getElementById("output").innerHTML=("You were born on Saturday and your Akan name is "+maleNames[6]);
+}
+
+
+
+
+if (d==0 && gender=="female") {
+
+  document.getElementById("output").innerHTML=("You were born on Sunday and your Akan name is "+femaleNames[0]);
+}
+if (d==1 && gender=="female") {
+
+  document.getElementById("output").innerHTML=("You were born on Monday and your Akan name is "+femaleNames[1]);
+}
+if (d==2 && gender=="female") {
+
+  document.getElementById("output").innerHTML=("You were born on Tuesday and your Akan name is "+femaleNames[2]);
+}
+if (d==3 && gender=="female") {
+
+  document.getElementById("output").innerHTML=("You were born on Wednesday and your Akan name is "+femaleNames[3]);
+}
+if (d==4 && gender=="female") {
+
+  document.getElementById("output").innerHTML=("You were born on Thursday and your Akan name is "+femaleNames[4]);
+}
+if (d==5 && gender=="female") {
+
+  document.getElementById("output").innerHTML=("You were born on Friday and your Akan name is "+femaleNames[5]);
+}
+if (d==6 && gender=="female") {
+
+  document.getElementById("output").innerHTML=("You were born on Saturday and your Akan name is "+femaleNames[6]);
+}
+
+
+}
+
