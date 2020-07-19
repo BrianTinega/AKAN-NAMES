0731fcdb6cb98f347d44616384b88e637093e03e
Brian Tinega Nyang'wara <briantinega@gmail.com>
2020-07-19 17:47:54 +0300

    Add cards to index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,36 @@
-var CC=parseInt(document.getElementById("yob").innerHTML.slice(0,2))
-  var YY=parseInt(document.getElementById("yob").innerHTML.slice(2,4))
-  var MM=parseInt(document.getElementById("mob").innerHTML)
-  var DD=parseInt(document.getElementById("dob").innerHTML)
-  var daysOfTheWeek=["sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
-var calculate=function(){
-  dayOfTheWeek= ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7
-  if (dayOfTheWeek===0){
-    alert("You were born on sunday")
-  }
-  else if (dayOfTheWeek===1){
-      alert("You were born on Monday")
-    }
-    else if (dayOfTheWeek===2){
-      alert("You were born on Tuesday")
-    }
-    else if (dayOfTheWeek===3){
-      alert("You were born on Wednesday")
-    }
-    else if (dayOfTheWeek===4){
-      alert("You were born on Thursday")
-    }
-    else if (dayOfTheWeek===5){
-      alert("You were born on Friday")
-    }
-    else if (dayOfTheWeek===6){
-      alert("You were born on Saturday")
-    }
+
+function calculateDay() {
+  var year=document.getElementById("yob")
+var month=parseInt(document.getElementById("mob"))
+var day=parseInt(doument.getElementById("dob"))
+var daysofTheWeek=["Sunday","Monday","Tuesday","Wedneday","Thursday","Friday","Saturday"]
+  var CC=parseInt((year.subString(0,2))+1)
+  var YY=parseInt(year.subString(2,4))
+  var MM=month
+  var DD=day
+  d = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7
+  console.log(d)
+  Math.floor(d);
 }
+if (d===0) {
+  alert("You were born on"+ daysofTheWeek[0] )
+}
+else if (d===1) {
+  alert("You were born on"+ daysofTheWeek[1])
+}
+else if (d===2) {
+  alert("You were born on"+ daysofTheWeek[2])
+}
+else if (d===3) {
+  alert("You were born on"+ daysofTheWeek[3])
+}
+else if (d===4) {
+  alert("You were born on"+ daysofTheWeek[4])
+}
+else if (d===5) {
+  alert("You were born on"+ daysofTheWeek[5])
+}
+else if (d===6) {
+  alert("You were born on"+ daysofTheWeek[6])
+}
+calculateDay()
